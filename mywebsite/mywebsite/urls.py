f075b8857df3103d752b39bd157876425e6d7f7a
Muhkahfims <114171924+Muhkahfims@users.noreply.github.com>
2024-02-28 22:19:49 +0800

    pengenalan app

--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -18,8 +18,12 @@ from django.contrib import admin
 from django.urls import path
 
 from . import views
+from blog import views as blogViews
+from about import views as aboutViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('blog/', blogViews.index),
+    path('about/', aboutViews.index),
 ]
